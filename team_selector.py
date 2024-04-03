0de10b0cdf373e568ff514635ebb02d3e372d650
Tori <130793421+toricason@users.noreply.github.com>
2024-04-03 15:17:29 -0400

    Add files via upload

--- team_selector.py
+++ team_selector.py
@@ -11,20 +11,33 @@ while True:
 
     random.shuffle(players)
 
-    team1 = players[:len(players)//2]
+    response = input('Is it a team or individual sport? \
+                     \nType team or individual: ')
 
-    print('Team 1 captain: ' + random.choice(team1))
-    print('Team 1: ')
-    for player in team1:
-        print(player)
+    if response == 'team':
 
-    team2 = players[len(players)//2:]
+        team1 = players[:len(players)//2]
 
-    print('\nTeam 2 captain: ' + random.choice(team2))
-    print('Team 2: ')
-    for player in team2:
-        print(player)
+        print('Team 1 captain: ' + random.choice(team1))
+        print('Team 1: ')
+        for player in team1:
+            print(player)
 
-    response = input('Pick team again? Type y or n: ')
-    if response == 'n':
-        break
+        team2 = players[len(players)//2:]
+
+        print('\nTeam 2 captain: ' + random.choice(team2))
+        print('Team 2: ')
+        for player in team2:
+            print(player)
+
+    else:
+        for i in range(0, 20, 2):
+            print(players[i] + ' vs ' + players[i+1])
+
+            start = random.randrange(i, i+2)
+
+            print(players[start] + ' starts')
+                
+        response = input('Pick team again? Type y or n: ')
+        if response == 'n':
+            break
